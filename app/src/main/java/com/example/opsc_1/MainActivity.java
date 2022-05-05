e0444ab4c9640df34f9c1d73e19965be10c92acf
RikoWolhuter <stormzy56@gmail.com>
2022-05-05 20:27:34 +0200

    Fifth commit

--- app/src/main/java/com/example/opsc_1/MainActivity.java
+++ app/src/main/java/com/example/opsc_1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.opsc_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+            getMenuInflater().inflate(R.menu.menu,menu);
+
+            MenuItem menuItem = menu.findItem(R.id.action_search);
+            SearchView searchView = (SearchView) menuItem.getActionView();
+            searchView.setQueryHint("Type here to search");
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {//Once a user has clicked submit on a search, it will search for item.
+
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    return false;
+                }
+            });
+            return super.onCreateOptionsMenu(menu);
     }
 }
