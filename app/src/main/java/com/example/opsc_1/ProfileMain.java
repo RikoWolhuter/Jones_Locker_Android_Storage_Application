cd1abedc2f70c7bc02714629b9203f0564fc8b7f
KarmenFabel <karmenfabel@gmail.com>
2022-05-17 14:23:32 +0200

    Profile Ui

---
+++ app/src/main/java/com/example/opsc_1/ProfileMain.java
@@ -0,0 +1,13 @@
+package com.example.opsc_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_main);
+    }
+}
