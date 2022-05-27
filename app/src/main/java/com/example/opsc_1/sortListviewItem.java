9f6fdb5f9119f5eddcb98f126620d4a25c1049b7
RikoWolhuter <stormzy56@gmail.com>
2022-05-27 13:25:52 +0200

    31th - Add menu navigation to the sort listview layouts and java code for it.

--- app/src/main/java/com/example/opsc_1/sortListviewItem.java
+++ app/src/main/java/com/example/opsc_1/sortListviewItem.java
@@ -1,4 +1,78 @@
 package com.example.opsc_1;
 
-public class sortListviewItem {
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class sortListviewItem extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch(item.getItemId()){
+            case R.id.nav_photo:
+                IntentHelper.openIntent(this, AddPhoto.class);
+                break;
+            case R.id.nav_collection:
+                IntentHelper.openIntent(this, Collection.class);
+                break;
+            case R.id.nav_graph:
+                IntentHelper.openIntent(this, Graph.class);
+                break;
+            case R.id.nav_progression:
+                IntentHelper.openIntent(this, Goal.class);
+                break;
+            case R.id.nav_profile:
+                IntentHelper.openIntent(this, Profile.class);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        //returning true marks the item as selected
+        return true;
+    }
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggleOnOff;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sort_listview_items);
+
+        toolbar = findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggleOnOff = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggleOnOff);
+        toggleOnOff.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed(){
+        //if the drawer is open, close it
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            //otherwise, let the super class handle it
+            super.onBackPressed();
+        }
+    }
 }
