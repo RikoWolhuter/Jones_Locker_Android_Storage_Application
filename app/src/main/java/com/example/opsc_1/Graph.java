38b12f0e0ffc65083ed459c29c2e8d84a3c99b47
RikoWolhuter <stormzy56@gmail.com>
2022-06-22 10:46:21 +0200

    40th - Added graph layout and temporary code for graph to see how it works.

--- app/src/main/java/com/example/opsc_1/Graph.java
+++ app/src/main/java/com/example/opsc_1/Graph.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.BarGraphSeries;
 
 public class Graph extends AppCompatActivity {
+
+    GraphView graphView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,34 @@ public class Graph extends AppCompatActivity {
         setContentView(R.layout.graph);
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[]{
+                // on below line we are adding
+                // each point on our x and y axis.
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+
+        // after adding data to our line graph series.
+        // on below line we are setting
+        // title for our graph view.
+        graphView.setTitle("My Graph View");
+
+        // on below line we are setting
+        // text color to our graph view.
+        graphView.setTitleColor(R.color.purple_200);
+
+        // on below line we are setting
+        // our title text size.
+        graphView.setTitleTextSize(18);
+
+        graphView.addSeries(series);
     }
 }
